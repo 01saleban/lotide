f3cb1ed1e5ff2f435c83186a25ee737af3b74a0f
Saleban Abdillahi <01saleban@gmail.com>
2021-11-26 02:39:27 +0000

    eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,16 @@
-const assertEqual = function(actual, expected) {
-  if ( actual === expected ) {
-      console.log ("Assertion Passed:");
-  }
-  else {
-      console.log ("Assertion Failed");
-  } 
-  };
-
-var eqArrays = function(arr1, arr2) {
-
-  for (var i = 0; arr1.length < i; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+const eqArrays = function(firstArr,secondArr) {
+  if (firstArr.length === secondArr.length) {
+    for (let i = 0; i < firstArr.length; i++) {
+      if (firstArr[i] !== secondArr[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
+  return false;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+
+
+module.exports = eqArrays;
