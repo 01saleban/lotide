7e49c44ff973d43f656f72f7aceaa72c321dbbf7
Saleban Abdillahi <01saleban@gmail.com>
2021-11-26 03:16:01 +0000

    letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,30 @@
+const assertArraysEqual = function(firstArr,secondArr) {
+  if (firstArr.length === secondArr.length) {
+    for (let i = 0; i < firstArr.length; i++) {
+      if (firstArr[i] !== secondArr[i]) {
+        return console.log(`❌❌❌ Assertion Failed: ${firstArr} !== ${secondArr}`);
+      }
+    }
+    return console.log(`✅✅✅ Assertion Passed: ${firstArr} === ${secondArr}`);
+  }
+  return console.log(`❌❌❌ Assertion Failed: ${firstArr} !== ${secondArr}`);
+  
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  const seen = [];
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions('hello').h, [0]);
+assertArraysEqual(letterPositions('hello').e, [1]);
+assertArraysEqual(letterPositions('hello').l, [2, 3]);
+assertArraysEqual(letterPositions('hello').o, [4]);
