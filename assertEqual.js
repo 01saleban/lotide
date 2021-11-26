3745d0afe4422272ac769e4867a99c7d3b5d4201
Saleban Abdillahi <01saleban@gmail.com>
2021-11-26 02:36:58 +0000

    assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if ( actual === expected ) {
-      console.log ("Assertion Passed:");
-  }
-  else {
-      console.log ("Assertion Failed");
-  }
+const assertArraysEqual = function(firstArr,secondArr) {
+    if (firstArr.length === secondArr.length) {
+      for (let i = 0; i < firstArr.length; i++) {
+        if (firstArr[i] !== secondArr[i]) {
+          return console.log(`❌❌❌ Assertion Failed: ${firstArr} !== ${secondArr}`);
+        }
+      }
+      return console.log(`✅✅✅ Assertion Passed: ${firstArr} === ${secondArr}`);
+    }
+    return console.log(`❌❌❌ Assertion Failed: ${firstArr} !== ${secondArr}`);
+    
   };
+  
+  module.exports = assertArraysEqual;
